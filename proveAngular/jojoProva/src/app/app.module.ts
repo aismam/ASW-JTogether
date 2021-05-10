cf827d1dca68a41ef0a1d4eef1ddb4a8e423c32a
Ismam <>
2021-05-10 11:10:54 +0200

    Cose

--- proveAngular/jojoProva/src/app/app.module.ts
+++ proveAngular/jojoProva/src/app/app.module.ts
@@ -4,24 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    NavbarComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- proveAngular/my-app/src/app/app.component.ts
+++ proveAngular/my-app/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import {User} from './_Models/users';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    // private authenticationService: AuthenticationService
+  ) {
+    // this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    this.router.navigate(['/login']);
+  }
+
+  logout(): void {
+    /*this.authenticationService.logout();
+    this.router.navigate(['/login']);*/
+  }
 }
