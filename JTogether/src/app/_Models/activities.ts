65f1105f19b7794ce42db0c363d0d412d7e62b1a
Ismam <>
2021-05-14 15:22:53 +0200

    Profilo senza cards

---
+++ JTogether/src/app/_Models/activities.ts
@@ -0,0 +1,12 @@
+export interface Activities {
+  imageUrl: string;
+  id: string;
+  name: string;
+  creator: string;
+  departurePlace: string;
+  arrivalPlace: string;
+  day: string;
+  time: string;
+  description: string;
+  participants: number;
+}
