5810cb20eef57c6ca03994740034190270b701ce
Ismam <>
2021-06-30 16:36:11 +0200

    fix in login.ts

--- JTogether/src/app/login/login.component.ts
+++ JTogether/src/app/login/login.component.ts
@@ -9,7 +9,6 @@ import {TokensManagerService} from '../tokens-manager.service';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit{
 
