ae2ceb4f876ed861e61b30b0caacaf6fb800e9c1
Ismam <>
2021-07-03 16:33:14 +0200

    profile pic fix

--- JTogether/src/app/profile-image.service.ts
+++ JTogether/src/app/profile-image.service.ts
@@ -5,7 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class ProfileImageService {
 
-  imageList = ['https://images.wallpaperscraft.com/image/astronaut_spacesuit_reflection_144426_1280x720.jpg',
+  // https://it.freepik.com/
+  // https://wallpaperscraft.com/
+
+  /*imageList = ['',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ];*/
+
+  // rand
+  /*imageList = ['https://images.wallpaperscraft.com/image/astronaut_spacesuit_reflection_144426_1280x720.jpg',
     'https://images.wallpaperscraft.com/image/city_vector_panorama_119914_2560x1080.jpg',
     'https://images.wallpaperscraft.com/image/minimalism_origami_japan_rising_sun_wave_74405_2560x1080.jpg',
     'https://images.wallpaperscraft.com/image/parrot_vector_drawing_bright_color_95908_2560x1080.jpg',
@@ -14,16 +29,32 @@ export class ProfileImageService {
     'https://images.wallpaperscraft.com/image/cloud_unicorn_rainbow_139591_2560x1080.jpg',
     'https://images.wallpaperscraft.com/image/owl_art_minimalism_vector_97418_1920x1200.jpg',
     'https://images.wallpaperscraft.com/image/kittens_printer_much_happiness_85662_1680x1050.jpg'
+  ];*/
+
+  // fiori
+  /*imageList = ['https://image.freepik.com/free-vector/flat-simple-geometric-elements_52683-56064.jpg',
+    'https://image.freepik.com/free-vector/geometric-mural-wallpaper_52683-48279.jpg',
+    'https://image.freepik.com/free-vector/flat-simple-geometric-elements_52683-56063.jpg',
+    'https://image.freepik.com/free-vector/geometric-groovy-pattern_52683-55782.jpg',
+    'https://image.freepik.com/free-vector/geometric-groovy-pattern_23-2148858302.jpg',
+    'https://image.freepik.com/free-vector/geometric-circular-groovy-seamless-pattern_23-2148825766.jpg',
+    'https://img.freepik.com/premium-vector/memphis-seamless-pattern-consisting-color-shapes-dark_111651-204.jpg?size=338&ext=jpg',
+    'https://image.freepik.com/free-vector/vintage-lines-geometric-groovy-seamless-pattern_23-2148837554.jpg',
+    'https://image.freepik.com/free-vector/hand-drawn-groovy-floral-pattern_23-2148836900.jpg',
+  ];*/
+
+  // prof pic
+  imageList = ['https://image.flaticon.com/icons/png/512/1018/1018961.png',
+    'https://image.flaticon.com/icons/png/512/1018/1018973.png',
+    'https://image.flaticon.com/icons/png/512/1176/1176205.png',
+    'https://image.flaticon.com/icons/png/512/1176/1176206.png',
+    'https://image.flaticon.com/icons/png/512/1176/1176207.png',
+    'https://image.flaticon.com/icons/png/512/1176/1176208.png',
+    'https://image.flaticon.com/icons/png/512/1018/1018979.png',
+    'https://image.flaticon.com/icons/png/512/1018/1018980.png',
+    'https://image.flaticon.com/icons/png/512/1019/1019128.png',
   ];
-  /* astronaut = 'https://images.wallpaperscraft.com/image/astronaut_spacesuit_reflection_144426_1280x720.jpg';
-  cityNight = 'https://images.wallpaperscraft.com/image/city_vector_panorama_119914_2560x1080.jpg';
-  japan = 'https://images.wallpaperscraft.com/image/minimalism_origami_japan_rising_sun_wave_74405_2560x1080.jpg';
-  parrot = 'https://images.wallpaperscraft.com/image/parrot_vector_drawing_bright_color_95908_2560x1080.jpg';
-  nightTree = 'https://images.wallpaperscraft.com/image/cat_art_tree_vector_118866_2048x1152.jpg';
-  deer = 'https://images.wallpaperscraft.com/image/deer_minimalism_camera_record_audio_cassette_yo_yo_vector_retro_99248_1680x1050.jpg';
-  rainbow = 'https://images.wallpaperscraft.com/image/cloud_unicorn_rainbow_139591_2560x1080.jpg';
-  owl = 'https://images.wallpaperscraft.com/image/owl_art_minimalism_vector_97418_1920x1200.jpg';
-  cats = 'https://images.wallpaperscraft.com/image/kittens_printer_much_happiness_85662_1680x1050.jpg'; */
+
 
   getRandomImage(): string {
     return this.imageList[Math.floor(Math.random() * 8) + 1];
