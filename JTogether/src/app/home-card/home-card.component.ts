ef56dd083cd4f1507d331e8c4961deb29ed2386a
Ismam <>
2021-07-03 16:16:03 +0200

    Aggiunte le immagini di profilo che vanno random

--- JTogether/src/app/home-card/home-card.component.ts
+++ JTogether/src/app/home-card/home-card.component.ts
@@ -1,15 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {DataService} from '../data.service';
 import {Router} from '@angular/router';
 import {TokensManagerService} from '../tokens-manager.service';
 import {SnackBarService} from '../snack-bar.service';
+import {ProfileImageService} from '../profile-image.service';
 
 @Component({
   selector: 'app-home-card',
   templateUrl: './home-card.component.html',
   styleUrls: ['./home-card.component.scss']
 })
-export class HomeCardComponent {
+export class HomeCardComponent implements OnInit{
   @Input() imageUrl: string | undefined;
   @Input() name: string | undefined;
   @Input() creator: string | undefined;
@@ -23,7 +24,8 @@ export class HomeCardComponent {
     private route: Router,
     private snackBar: SnackBarService,
     private dataService: DataService,
-    private tokenService: TokensManagerService
+    private tokenService: TokensManagerService,
+    private profileImage: ProfileImageService,
   ) { }
 
   participate(): void{
@@ -34,5 +36,10 @@ export class HomeCardComponent {
         this.isHidden = true;
       })
       .catch(e => this.snackBar.errorSnack(e.message));
+  }
+
+  ngOnInit(): void {
+    //this.imageUrl = 'https://images.wallpaperscraft.com/image/deer_minimalism_camera_record_audio_cassette_yo_yo_vector_retro_99248_1680x1050.jpg';
+    this.imageUrl = this.profileImage.getRandomImage();
   }
 }
