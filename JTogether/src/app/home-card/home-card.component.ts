3b58e78e1d6ee816273d090a866a427426a890cb
Lorenzo Gardini <lorenzo.gardini7@studio.unibo.it>
2021-07-05 17:08:19 +0200

    added distance to home cards

--- JTogether/src/app/home-card/home-card.component.ts
+++ JTogether/src/app/home-card/home-card.component.ts
@@ -5,6 +5,8 @@ import {TokensManagerService} from '../tokens-manager.service';
 import {SnackBarService} from '../snack-bar.service';
 import {ProfileImageService} from '../profile-image.service';
 import {UtilityService} from '../utility.service';
+import {GeolocationService} from "../geolocation-service";
+import {Geolocation} from "../_Models/Geolocation";
 
 @Component({
   selector: 'app-home-card',
@@ -15,11 +17,13 @@ export class HomeCardComponent implements OnInit{
   @Input() imageUrl: string | undefined;
   @Input() name: string | undefined;
   @Input() creator: string | undefined;
-  @Input() place: string | undefined;
+  @Input() location: string | undefined;
   @Input() dateTime: string | undefined;
   @Input() description: string | undefined;
   @Input() activityId: string | undefined;
-  isHidden = false;
+  @Input() geolocation: Geolocation | undefined;
+  distanceInMeters: number | undefined;
+  isVisible = true;
 
   constructor(
     private route: Router,
@@ -27,18 +31,20 @@ export class HomeCardComponent implements OnInit{
     private dataService: DataService,
     private tokenService: TokensManagerService,
     private profileImage: ProfileImageService,
-    private utilityService: UtilityService,
-  ) { }
+    private geolocationService: GeolocationService,
+    private utilityService: UtilityService) { }
+
+  ngOnInit(): void {
+    this.dateTime = this.utilityService.formatDateTime(this.dateTime as string);
+    this.geolocationService.getGeolocation()
+      .then(c => this.distanceInMeters = this.geolocationService.getDistance(this.geolocation as Geolocation, c));
+  }
 
   participate(): void{
     this.tokenService.getAccessToken()
       .then(t => this.dataService.createParticipation({activity_id: this.activityId}, t))
       .then(_ => this.snackBar.normalSnack('Adesso partecipi a ' + this.name + '!'))
-      .then(_ => this.isHidden = true)
+      .then(_ => this.isVisible = false)
       .catch(e => this.snackBar.errorSnack(e.message));
-  }
-
-  ngOnInit(): void {
-    this.dateTime = this.utilityService.formatDateTime(this.dateTime as string);
   }
 }
